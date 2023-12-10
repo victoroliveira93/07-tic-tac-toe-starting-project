7746f2bc3d03c2f7de46391f3f38d9595d4d8fac
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-12-09 21:04:21 -0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
